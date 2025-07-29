e9261f8171b053cdbd6ed12ce04854ab524fbe82
Kilian COQUELIN <kilian.coquelin@gmail.com>
2025-07-29 10:14:59 +0000

    Products cleaning

---
+++ models/staging/raw/stg_raw__product.sql
@@ -0,0 +1,19 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'product') }}
+
+),
+
+renamed as (
+
+    select
+        products_id,
+        (purchse_price) as purchase_cost
+
+    from source
+
+)
+
+select * from renamed
