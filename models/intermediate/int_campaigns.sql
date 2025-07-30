f4599c4d7135145068f140be64fee8d30c2ab902
Kilian COQUELIN <kilian.coquelin@gmail.com>
2025-07-30 12:21:30 +0000

    yu

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -3,8 +3,10 @@ FROM {{ref('stg_raw_adwords')}}
 UNION ALL
 SELECT *
 FROM {{ref('stg_raw_bing')}}
+UNION ALL
 SELECT *
 FROM {{ref('stg_raw_criteo')}}
 UNION ALL
 SELECT *
 FROM {{ref('stg_raw_facebook')}}
+UNION ALL
