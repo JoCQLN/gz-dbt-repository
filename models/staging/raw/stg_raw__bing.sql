1ef27b9ae7f7627e68c880cd65296ffa96a88767
Kilian COQUELIN <kilian.coquelin@gmail.com>
2025-07-30 10:53:25 +0000

    BING

--- models/staging/raw/stg_raw__bing.sql
+++ models/staging/raw/stg_raw__bing.sql
@@ -14,7 +14,6 @@ renamed as (
         campaign_key,
         (campgn_name) as campaign_name,
         CAST(ads_cost as FLOAT64) as ads_cost,
-        ads_cost,
         impression,
         click
 
