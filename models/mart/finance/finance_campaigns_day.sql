2850a52d3b7a09ab766df45933a7235e7e6a2c42
Kilian COQUELIN <kilian.coquelin@gmail.com>
2025-07-30 14:09:57 +0000

    Finance_campaign_days

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,31 +1,21 @@
- {{config(materialized='table')}}
- 
- WITH orders_per_day AS (
-   SELECT
-     date_
-     ,COUNT(DISTINCT orders_id) AS nb_transactions
-     ,ROUND(SUM(revenue),0) AS revenue
-     ,ROUND(SUM(margin),0) AS margin
-     ,ROUND(SUM(operational_margin),0) AS operational_margin
-     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
-     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
-     ,ROUND(SUM(logcost),0) AS log_cost
-     ,ROUND(SUM(ship_cost),0) AS ship_cost
-     ,SUM(quantity) AS quantity
- FROM {{ref("int_orders_operational")}}
- GROUP BY  date_
- )
-
- SELECT
-     date_
-     , revenue
-     , margin
-     , operational_margin
-     , purchase_cost
-     , shipping_fee
-     , log_cost
-     , ship_cost
-     , quantity
-     , ROUND(revenue/NULLIF(nb_transactions, 0), 2) AS average_basket
- FROM orders_per_day
- ORDER BY  date_ DESC
+SELECT
+  date_date,
+  ROUND(SUM(operational_margin-ads_cost),2)  AS ads_margin,
+  ROUND(SUM(average_basket),2)  AS average_basket,
+  ROUND(SUM(operational_margin),2) AS operational_margin,
+  --agregation des ads/imp/click
+  ROUND(SUM(ads_cost),2) AS ads_cost,
+  ROUND(SUM(impression),2) AS ads_impression,
+  ROUND(SUM(click),2) AS ads_click,
+   --fin des agregations
+  ROUND(SUM(quantity),2) AS quantity,
+  ROUND(SUM(revenue),2) AS revenue,
+  ROUND(SUM(purchase_cost),2) AS purchase_cost,
+  ROUND(SUM(margin),2) AS margin,
+  ROUND(SUM(shipping_fee),2) AS shipping_fee,
+  ROUND(SUM(log_cost),2) AS log_cost,
+  ROUND(SUM(ship_cost),2) AS ship_cost
+FROM{{ref('finance_days')}}
+INNER JOIN {{ref('int_campaigns_days')}}
+USING (date_date)
+GROUP BY date_date
