0c7c48b3e981bc7c4d3a89c11de8c48da0a454b7
Kilian COQUELIN <kilian.coquelin@gmail.com>
2025-07-30 07:06:13 +0000

    Finance_day_1

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         products_id,
-        (CAST(purchSE_PRICE as FLOAT64)) as purchase_cost
+        (CAST(purchSE_PRICE as FLOAT64)) as purchase_price
 
     from source
 
