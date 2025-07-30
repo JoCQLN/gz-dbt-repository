4aa9527c56894e812a752c72075305ab59cd51df
Kilian COQUELIN <kilian.coquelin@gmail.com>
2025-07-30 12:08:43 +0000

    FACEBOOK

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -12,8 +12,8 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        (campgn_name) as campaign_key,
-        CAST(ads_cost as FLOAT64),
+        (campgn_name) as campaign_name,
+        CAST(ads_cost as FLOAT64) as ads_cost,
         impression,
         click
 
